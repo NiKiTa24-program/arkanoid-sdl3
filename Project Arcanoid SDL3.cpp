74cd06e5d0f1b95b1c04746bae87c88a229e0391
Nikita <nk2411206@gmail.com>
2026-06-23 19:42:27 +0300

    v 1.1 ball speed

--- Project Arcanoid SDL3.cpp
+++ Project Arcanoid SDL3.cpp
@@ -14,13 +14,11 @@ typedef struct {
 int main() {
     setlocale(LC_ALL, "RU");
 
-    // Инициализация SDL
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         printf("Ошибка инициализации SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Создание окна
     SDL_Window* window = SDL_CreateWindow(
         "Arkanoid",
         WINDOW_WIDTH,
@@ -33,7 +31,6 @@ int main() {
         return 1;
     }
 
-    // Создание рендерера
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
     if (!renderer) {
         printf("Ошибка создания рендерера: %s\n", SDL_GetError());
@@ -55,79 +52,93 @@ int main() {
         20,
         20
     };
-    float ball_dx = 2.0f;
-    float ball_dy = -3.0f;
+
+    // Cкорость
+    float ball_dx = 1.0f;
+    float ball_dy = -1.5f;
+
+    // Флаг, чтобы мяч не двигался до первого клика
+    bool ballMoving = false;
 
     SDL_Event event;
     bool running = true;
 
     while (running) {
-        // 1. ОБРАБОТКА СОБЫТИЙ
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 running = false;
             }
             if (event.type == SDL_EVENT_MOUSE_MOTION) {
                 paddle.x = event.motion.x - paddle.w / 2;
-
-                // Ограничение, чтобы платформа не выходила за экран
                 if (paddle.x < 0) paddle.x = 0;
                 if (paddle.x + paddle.w > WINDOW_WIDTH) {
                     paddle.x = WINDOW_WIDTH - paddle.w;
                 }
             }
+            // Запуск мяча по клику мыши
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                if (!ballMoving) {
+                    ballMoving = true;
+                }
+            }
+            // Или по нажатию клавиши ПРОБЕЛ
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_SPACE) {
+                    if (!ballMoving) {
+                        ballMoving = true;
+                    }
+                }
+            }
         }
 
-        // 2. ОБНОВЛЕНИЕ ЛОГИКИ (ВНЕ обработки событий)
-        ball.x += ball_dx;
-        ball.y += ball_dy;
+        // ДВИЖЕНИЕ МЯЧА ТОЛЬКО ЕСЛИ ballMoving
+        if (ballMoving) {
+            ball.x += ball_dx;
+            ball.y += ball_dy;
 
-        // Отскок от левой и правой стен
-        if (ball.x < 0 || ball.x + ball.w > WINDOW_WIDTH) {
-            ball_dx = -ball_dx;
+            // Отскок от стен
+            if (ball.x < 0 || ball.x + ball.w > WINDOW_WIDTH) {
+                ball_dx = -ball_dx;
+            }
+            if (ball.y < 0) {
+                ball_dy = -ball_dy;
+            }
+            if (ball.y + ball.h > WINDOW_HEIGHT) {
+                printf("Game Over!\n");
+                running = false;
+            }
+
+            // Отскок от платформы
+            if (ball_dy > 0 &&
+                ball.x < paddle.x + paddle.w &&
+                ball.x + ball.w > paddle.x &&
+                ball.y + ball.h >= paddle.y &&
+                ball.y + ball.h <= paddle.y + paddle.h) {
+                ball_dy = -ball_dy;
+                ball.y = paddle.y - ball.h;
+            }
+        }
+        else {
+            // Мяч привязан к платформе (ждём старта)
+            ball.x = paddle.x + paddle.w / 2 - ball.w / 2;
+            ball.y = paddle.y - ball.h - 5;
         }
 
-        // Отскок от верхней стены
-        if (ball.y < 0) {
-            ball_dy = -ball_dy;
-        }
-
-        // Проверка проигрыша (мяч упал вниз)
-        if (ball.y + ball.h > WINDOW_HEIGHT) {
-            printf("Game Over!\n");
-            running = false;
-        }
-
-        // Отскок от платформы
-        if (ball_dy > 0 &&
-            ball.x < paddle.x + paddle.w &&
-            ball.x + ball.w > paddle.x &&
-            ball.y + ball.h >= paddle.y &&
-            ball.y + ball.h <= paddle.y + paddle.h) {
-            ball_dy = -ball_dy;
-            ball.y = paddle.y - ball.h;
-        }
-
-        // 3. РИСОВАНИЕ
-        // Очистка экрана (чёрный фон)
+        // РИСОВАНИЕ
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Рисуем платформу
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_FRect rect = { paddle.x, paddle.y, paddle.w, paddle.h };
         SDL_RenderFillRect(renderer, &rect);
 
-        // Рисуем мяч
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_FRect ballRect = { ball.x, ball.y, ball.w, ball.h };
         SDL_RenderFillRect(renderer, &ballRect);
 
-        // Показываем на экране
         SDL_RenderPresent(renderer);
     }
 
-    // Очистка
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
